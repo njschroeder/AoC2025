7e9cd7e9b617a1b93d7d0001b773e15f0472fd9e
Nick Schroeder <65873597+njschroeder@users.noreply.github.com>
2025-12-03 21:42:01 -0600

    Functional Part 2

--- Day3.py
+++ Day3.py
@@ -2,6 +2,8 @@ input = open('C:\\Users\\[USER]\\Downloads\\2025aoc3data.txt', 'r').read()
 
 joltages = input.split()
 
+
+#### PART 1 ####
 total = 0
 for joltage in joltages:
     first = 0
@@ -13,4 +15,17 @@ for joltage in joltages:
         if int(index) > second: # save the value if it beats our second digit, this increases our joltage
             second = int(index)
     total += 10*first + second # merge the two into one number and add it in
+print(total)
+
+
+#### PART 2 ####
+total = 0
+for string in joltages:
+    joltage = list(map(int, string))
+    output = [0] * 12
+    start = 0
+    for i in range(12):
+        maximum = max(joltage[start:len(joltage)-11+i]) # Run from after last filled in digit to the last point far enough from the end that we can copy remaining list if needed
+        start = joltage.index(maximum, start) + 1 # set system to check from just after the last value punched in
+        total += maximum * 10**(11-i) # add max value to total, adjusted for its power of 10
 print(total)
